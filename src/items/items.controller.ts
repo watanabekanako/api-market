b6d91255870b357d81355ad5bbd2ece45165cbfe
“Kanako_Watanabe” <109854906+watanabekanako@users.noreply.github.com>
2023-02-15 11:28:30 +0900

    CRUD作成　Postmanにて確認完了

---
+++ src/items/items.controller.ts
@@ -0,0 +1,50 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Param,
+  Patch,
+  Delete,
+} from '@nestjs/common';
+import { ItemsService } from './items.service';
+import { Item } from './item.model';
+import { ItemStatus } from './item-status.enum';
+@Controller('items')
+export class ItemsController {
+  constructor(private readonly itemsService: ItemsService) {}
+  @Get()
+  findAll(): Item[] {
+    return this.itemsService.findAll();
+  }
+  @Get(':id')
+  findById(@Param('id') id: string): Item {
+    return this.itemsService.findById(id);
+  }
+  @Post()
+  create(
+    @Body('id') id: string,
+    @Body('name') name: string,
+    @Body('price') price: number,
+    @Body('description') description: string,
+  ): Item {
+    const item: Item = {
+      id,
+      name,
+      price,
+      description,
+      status: ItemStatus.ON_SALE,
+    };
+    return this.itemsService.create(item);
+  }
+
+  @Patch(':id')
+  updateStatus(@Param('id') id: string): Item {
+    return this.itemsService.updateStatus(id);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string): void {
+    this.itemsService.delete(id);
+  }
+}
